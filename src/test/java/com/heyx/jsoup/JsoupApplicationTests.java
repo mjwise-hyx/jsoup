c5acb798104b945b346d8c5b3079e566c034d70b
1064042411@qq.com <1064042411>
2019-04-11 18:31:14 +0800

    数据解析

--- src/test/java/com/heyx/jsoup/JsoupApplicationTests.java
+++ src/test/java/com/heyx/jsoup/JsoupApplicationTests.java
@@ -1,16 +1,28 @@
 package com.heyx.jsoup;
 
+import com.heyx.jsoup.service.BaseService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JsoupApplicationTests {
 
+    @Autowired
+    BaseService baseService;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws IOException {
+        Document doc = Jsoup.connect("http://kaijiang.500.com/ssq.shtml").get();
+
+        baseService.parseDocument(doc);
     }
 
 }
